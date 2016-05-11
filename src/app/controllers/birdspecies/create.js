0a939757da5b72dd861370056d47d801bac3b63d
Stephen Fuqua <stephen@safnet.com>
2016-05-10 21:23:59 -0500

    Everything except bird surveys seems to be working now.

--- src/app/controllers/birdspecies/create.js
+++ src/app/controllers/birdspecies/create.js
@@ -1,18 +1,33 @@
 'use strict';
 
 flnd.birdSpeciesCreate = {
+    updateSurveyTypes: function(birdspecies) {
+        // This hard-coding is nasty but useful due to time constraints
+        birdspecies.surveyTypeNames = [];
+        if (birdspecies.surveyTypeRookery) {
+            birdspecies.surveyTypeNames.push('TERN Rookery Survey');
+        }
+        if (birdspecies.surveyTypeForaging) {
+            birdspecies.surveyTypeNames.push('TERN Waterbird Foraging Survey');
+        }
+    },
+
     configureSubmit: function(config, $scope, messenger, authService, $uibModalInstance) {
-        return function () {
+        var $this = this;
+
+        return function() {
             $scope.loading = true;
+
+            $this.updateSurveyTypes($scope.birdspecies);
 
             authService.post(config.birdspecies, $scope.birdspecies)
                 .then(function success() {
 
-                $uibModalInstance.close();
+                    $uibModalInstance.close();
                 }, function error(response) {
                     messenger.displayErrorResponse($scope, response);
                 })
-                .finally(function(){
+                .finally(function() {
                     $scope.loading = false;
                 });
         };
@@ -27,24 +42,24 @@ flnd.birdSpeciesCreate = {
  * Controller for the create bird species page.
  */
 angular.module('flightNodeApp')
-    .controller('BirdSpeciesCreateController',
-        ['$scope', '$http', '$log', '$location', 'messenger', 'authService', 'config', '$uibModalInstance',
-            function ($scope, $http, $log, $location, messenger, authService, config, $uibModalInstance) {
+    .controller('BirdSpeciesCreateController', ['$scope', '$http', '$log', '$location', 'messenger', 'authService', 'config', '$uibModalInstance',
+        function($scope, $http, $log, $location, messenger, authService, config, $uibModalInstance) {
 
-                if (!(authService.isAdministrator() ||
-                      authService.isCoordinator())) {
-                    $log.warn('not authorized to access this path');
-                    $location.path('/');
-                    return;
-                }
+            if (!(authService.isAdministrator() ||
+                    authService.isCoordinator())) {
+                $log.warn('not authorized to access this path');
+                $location.path('/');
+                return;
+            }
 
-                $scope.loading = true;
+            $scope.loading = true;
 
-                $scope.cancel = function () {
-                    $uibModalInstance.dismiss('cancel');
-                };
+            $scope.cancel = function() {
+                $uibModalInstance.dismiss('cancel');
+            };
 
-                $scope.submit = flnd.birdSpeciesCreate.configureSubmit(config, $scope, messenger, authService, $uibModalInstance);
+            $scope.submit = flnd.birdSpeciesCreate.configureSubmit(config, $scope, messenger, authService, $uibModalInstance);
 
-                $scope.loading = false;
-            }]);
+            $scope.loading = false;
+        }
+    ]);
